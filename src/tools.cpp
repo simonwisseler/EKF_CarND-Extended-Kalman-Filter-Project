63871af036a583145c48924b8843f4c1ce7f3940
simonwisseler <simonwisseler@gmail.com>
2019-03-07 15:58:10 +0100

    Add Tools::CalculateJacobian and correct Tools::CalculateRMSE

--- src/tools.cpp
+++ src/tools.cpp
@@ -16,17 +16,42 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     
     for(unsigned i = 0; i < estimations.size(); i++){
         VectorXd residual = estimations[i] - ground_truth[i];
-        residual = residual.array()*residual.array() //element-wise multiplication
+        residual = residual.array()*residual.array(); //element-wise multiplication
         rmse += residual;
     }
-    rmse = residual/estimations.size();
+    rmse = rmse/estimations.size();
     rmse = rmse.array().sqrt();
-    return rmse
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-   * TODO:
-   * Calculate a Jacobian here.
-   */
+    /**
+     * Jacobian used to calculate predicted radar measurements - first-order Taylor expansion
+     * required to map state x from Cartesian coordinates px, py, vx, vy to Polar coordinates rho, phi, rho_dot
+     *
+     * NOTE: For mathematical derivation of range rate rho_dot, see https://en.wikipedia.org/wiki/Range_rate
+     * NOTE: To arrive at partial derivatives of rho_dot with respect to px, py,
+     * use the product rule and expand the resulting first summand/fraction by multiplying
+     * numerator and denominator by (px^2+py^2)
+     */
+    
+    MatrixXd Hj(3, 4);
+    
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+    
+    if(px == 0 && py == 0) {
+        std::cout << "Error: Division by 0 in Tools::CalculateJacobian" << std::endl;
+        return Hj;
+    }
+    float rho = std::sqrt(px*px+py*py);
+    float rho_squared = rho*rho;
+    float rho_cubed = rho_squared*rho;
+    Hj << px/rho, py/rho, 0, 0,
+        -py/rho_squared, px/rho_squared,0, 0,
+        py*(vx*py-vy*px)/rho_cubed, px*(vy*px-vx*py)/rho_cubed, px/rho, py/rho;
+    return Hj;
+    
 }
--- src/tools.h
+++ src/tools.h
@@ -7,23 +7,23 @@
 class Tools {
  public:
   /**
-   * Constructor.
+   * Constructor
    */
   Tools();
 
   /**
-   * Destructor.
+   * Destructor
    */
   virtual ~Tools();
 
   /**
-   * A helper method to calculate RMSE.
+   * A helper method to calculate RMSE
    */
   Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
                                 const std::vector<Eigen::VectorXd> &ground_truth);
 
   /**
-   * A helper method to calculate Jacobians.
+   * A helper method to calculate Jacobians
    */
   Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
 

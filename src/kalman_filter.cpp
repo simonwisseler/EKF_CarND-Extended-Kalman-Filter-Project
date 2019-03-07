5b74c6319342bfc308246c96e4e837d51967ed27
simonwisseler <simonwisseler@gmail.com>
2019-03-07 17:33:58 +0100

    Add KalmanFilter::Predict() and KalmanFilter::Update() in kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -14,28 +14,37 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  /**
-   * TODO: predict the state
-   */
+    /**
+     * NOTE: No linear approximation of F_ required because of linear motion model
+     */
+    x_ = F_*x_;
+    MatrixXd F_t = F_.transpose();
+    P_ = F_*P_*F_t + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Kalman Filter equations
-   */
+    MatrixXd H_t = H_.transpose();
+    MatrixXd S = H_*P_*H_t + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd K = P_*H_t*Si;
+    MatrixXd I = MatrixXd::Identity(4,4);
+    
+    VectorXd z_pred = H_*x_;
+    VectorXd info_gain = z - z_pred;
+    
+    x_ = x_ + (K*info_gain);
+    P_ = (I - K*H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-   * TODO: update the state by using Extended Kalman Filter equations
-   */
+    
 }
